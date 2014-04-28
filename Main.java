7960be541633ffa664c6e08779d4669d505d7b55
ilyes <interistani@gmail.com>
2014-04-28 12:40:11 -0400

    First commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    String ch ;
+    int  x ;
+
+    public void affiche(){
+        System.out.println(ch);
+		System.out.println(x);
+    }
+
+    public void setCh(String ch) {
+        this.ch = ch;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+	public Main(String c,int t)
+	{
+	ch=c;
+	x=t;
+	}
+}
